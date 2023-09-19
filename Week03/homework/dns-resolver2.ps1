83c8aea06660ed45b67bdddd75cb3419780caa61
Jacob-Mayotte <71035380+Jacob-Mayotte@users.noreply.github.com>
2023-09-18 23:26:06 -0400

    Add PS script

---
+++ Week03/homework/dns-resolver2.ps1
@@ -0,0 +1,9 @@
+﻿param($network, $dns)
+
+$hostid = 0..255
+
+foreach($id in $hostid){
+
+Resolve-DnsName -Name "$network.$id" -Server "$dns" -ErrorAction SilentlyContinue | Select-Object Name, Namehost
+
+}
